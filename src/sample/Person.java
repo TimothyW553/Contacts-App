5d4fdb87231c25eb9f6e2559825f1e474b584f01
Timothy Wang <timothywang@crescentschool.org>
2019-11-29 13:54:25 -0500

    Update Person.java

--- src/sample/Person.java
+++ src/sample/Person.java
@@ -21,42 +21,52 @@ public class Person {
         this.address = new SimpleStringProperty(address); // initialize address
     }
 
+    // Accessor Method for getting first name
     public String getFirstName() {
         return firstName.get();
     }
 
+    // Mutator Method for changing first name
     public void setFirstName(String fName) {
         firstName.set(fName);
     }
 
+    // Accessor Method for getting last name
     public String getLastName() {
         return lastName.get();
     }
 
+    // Mutator Method for changing last name
     public void setLastName(String fName) {
         lastName.set(fName);
     }
 
+    // Accessor Method for getting email
     public String getEmail() {
         return email.get();
     }
 
+    // Mutator Method for changing email
     public void setEmail(String fName) {
         email.set(fName);
     }
 
+    // Accessor Method for getting phone number
     public String getPhoneNumber() {
         return phoneNumber.get();
     }
 
+    // Mutator Method for changing phone number
     public void setPhoneNumber(String pNumber) {
         phoneNumber.set(pNumber);
     }
 
+    // Accessor Method for getting address
     public String getAddress() {
         return address.get();
     }
 
+    // Mutator Method for changing address
     public void setAddress(String loc) {
         address.set(loc);
     }
